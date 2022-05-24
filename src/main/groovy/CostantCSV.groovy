1b44e3c913e756e375baac704a5eed5aa154b494
FGranelli <f.granelli@ads.it>
2022-05-24 16:17:58 +0200

    Modifiche Campi

--- src/main/groovy/CostantCSV.groovy
+++
@@ -1,8 +0,0 @@
-class CostantCSV {
-    private CostantCSV(){}
-    public static final String HEADER_FILE ="..//resources//HeaderDemo1.csv"
-    public static final String DISPOSIZIONI_FILE = "..//resources//VersamentiDemo1.csv"
-    public static final String FILE_OUT = "out.xml"
-    public static final String FILE_XSD_VALIDATOR ="CBIBdyPaymentRequest.00.04.00.xsd"
-
-}
